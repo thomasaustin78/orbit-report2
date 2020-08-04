444dc85633cc4af351300382ca2d65f81827fef4
Austin Thomas <thomasaustin78@gmail.com>
2020-08-03 19:01:24 -0500

    finished step 4 added table of satellites

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -5,11 +5,11 @@ type: string;
 operational: boolean;
 launchDate: string;
 constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
-    name = this.name;
-    type = this.type;
-    launchDate = this.launchDate;
-    orbitType = this.orbitType;
-    operational = this.operational;
+    this.name= name;
+    this.type = type;
+    this.launchDate = launchDate;
+    this.orbitType = orbitType;
+    this.operational = operational;
 }
 }
 
